52e29c18776528a99700e50b6bf3171214e229b8
lucaspontoexe <ltx16@hotmail.com>
2019-11-28 15:57:03 -0300

    adicionar tabela e botões pelo teste

--- front/src/pages/ListaProdutos/index.jsx
+++ front/src/pages/ListaProdutos/index.jsx
@@ -4,6 +4,28 @@ export default function ListaProdutos() {
     return (
         <div>
             uma lista de produtos aparece aqui
+
+            <button>botão 1</button>
+            <button>botão 2</button>
+
+
+            <div className="table">
+                <tr>
+                    <th>Cabeçalho A</th>
+                    <th>algo B</th>
+                    <th>coisa C</th>
+                </tr>
+                <tr>
+                    <td>Teste 1</td>
+                    <td>Isso vai ser gerado por código</td>
+                    <td>object.keys nesse negócio</td>
+                </tr>
+                <tr>
+                    <td>Teste 2</td>
+                    <td>Isso vai ser gerado por código</td>
+                    <td>map neles</td>
+                </tr>
+            </div>
         </div>
     )
 }
