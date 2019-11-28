30289ac23c4da5b8d0ed64f32296188566821033
lucaspontoexe <ltx16@hotmail.com>
2019-11-28 14:01:52 -0300

    Formatação de código

--- front/src/routes.js
+++ front/src/routes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Teste from './pages/Teste';
+import Teste from "./pages/Teste";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={Teste} />
-        </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Teste} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
