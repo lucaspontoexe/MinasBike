4e41bf31f56226e08c8502bb8ac3a27cc8ce82bc
lucaspontoexe <ltx16@hotmail.com>
2019-11-29 11:17:33 -0300

    add styled components

---
+++ front/src/components/Button.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background: #c4c4c4;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    border: none;
+
+    height: 48px;
+    width: 160px;
+    margin: 20px;
+
+    /* text properties */
+    color: white;
+    font-size: 24px;
+    line-height: 28px;
+    /* font family */
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
+
+export default Button;
--- front/src/pages/Login/index.jsx
+++ front/src/pages/Login/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Button from '../../components/Button';
 
 export default function Login({ history }) {
     return (
         <div>
             Login
-            <button onClick={() => history.push('/lista')}>botão</button>
+            <Button onClick={() => history.push('/lista')}>botão</Button>
         </div>
     );
 }
