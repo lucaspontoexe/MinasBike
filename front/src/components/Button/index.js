59a370a395293d57e2aabc62183ae3c119666d33
lucaspontoexe <ltx16@hotmail.com>
2019-11-29 19:35:22 -0300

    button styling without styled component

--- front/src/components/Button/index.js
+++ front/src/components/Button/index.js
@@ -1,25 +1,9 @@
-// styled-components foi um teste. dá pra usar só CSS tranquilamente.
-import styled from 'styled-components';
+import React from 'react'
+import 'style.css'
 
-const Button = styled.button`
-    /* color, general styling */
-    background: ${props => props.color || '#c4c4c4'};
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
-    border: none;
+export default function Button({children, color, textColor, onClick}) {
+    return (
 
-    /* positioning */
-    height: 48px;
-    min-width: 160px;
-    margin: 20px;
-    padding: 0px 20px;
-
-    /* text properties */
-    color: ${props => props.textColor || 'white'};
-    font-size: 24px;
-    line-height: 28px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-`;
-
-export default Button;
+    <button style={{backgroundColor: color, color: textColor}} onClick={onClick}>{children}</button>
+    )
+}
