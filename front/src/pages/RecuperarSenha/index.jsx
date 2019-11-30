e306ebd68b4eaefd1f524e04835ddf0c6aab3418
lucaspontoexe <ltx16@hotmail.com>
2019-11-30 17:08:07 -0300

    easter egg removido ou não

--- front/src/pages/RecuperarSenha/index.jsx
+++ front/src/pages/RecuperarSenha/index.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 export default function RecuperarSenha() {
     return (
         <div>
-            <p>
-                bom, aí você vai ter que pegar algum remédio ou usar um método
-                alternativo de recuperação de memória.
-            </p>
-            <p>o quê, achou que eu ia mudar sua senha? achou errado</p>
+            <h1>Recuperar Senha</h1>
         </div>
     );
 }
