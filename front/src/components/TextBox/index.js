2941e1fc387515274f797150c3d1d9c113034d65
lucaspontoexe <ltx16@hotmail.com>
2019-11-30 19:45:36 -0300

    TextBox: add autocomplete support

--- front/src/components/TextBox/index.js
+++ front/src/components/TextBox/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './style.css';
 
 export default function TextBox(props) {
+    const { label, name, options, list } = props;
     return (
         <div className="text-box">
             {/* renderiza label se existir */}
-            {props.label && <label htmlFor={props.name}>{props.label}</label>}
+            {label && <label htmlFor={name}>{label}</label>}
             {/* copia todas as props pro input */}
             <input {...props}></input>
+
+            {list && (
+                <datalist id={list}>
+                    {options.map(option => (
+                        <option value={option} />
+                    ))}
+                </datalist>
+            )}
         </div>
     );
 }
--- front/src/pages/CadastroProdutos/index.jsx
+++ front/src/pages/CadastroProdutos/index.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TextBox from '../../components/TextBox';
 
 export default class CadastroProdutos extends Component {
     render() {
         return (
             <div>
                 Tela de Cadastro
+                <TextBox
+                    label="hmm"
+                    list="teste"
+                    options={['Carro', 'Moto', 'Bicicleta', 'Qualquer Coisa']}
+                />
             </div>
-        )
+        );
     }
 }
