b867cdfbda6609e3e7e3adbfa63c1e856a18f9bd
Rebeca Nonato Silva <rebecanonato89@gmail.com>
2019-12-04 08:52:31 -0300

    mensagem de erro login

--- front/src/pages/Login/index.jsx
+++ front/src/pages/Login/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from '../../components/Button';
 import TextBox from '../../components/TextBox';
+import Error from '../../components/Error';
 
 import api from '../../services/api';
 import './styles.css';
@@ -8,6 +9,7 @@ import './styles.css';
 export default function Login({ history }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -22,36 +24,42 @@ export default function Login({ history }) {
                 history.push('/lista');
             })
 
-            .catch(error => console.log(error.response));
+            .catch(err => {
+                setError(err.response.request.response);
+            });
     }
 
+
     return (
-        <div className="login-screen">
-            <form onSubmit={handleSubmit}>
-                <TextBox
-                    name="user"
-                    type="email"
-                    label="Login"
-                    placeholder="user@example.com"
-                    required
-                    autoFocus
-                    value={email}
-                    onChange={event => setEmail(event.target.value)}
-                />
+        <>
+            {error && <Error>{error}</Error>}
+            <br />
+            <div className="login-screen">
+                <form onSubmit={handleSubmit}>
+                    <TextBox
+                        name="user"
+                        type="email"
+                        label="Login"
+                        placeholder="user@example.com"
+                        required
+                        autoFocus
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                    />
 
-                <TextBox
-                    name="password"
-                    type="password"
-                    label="Senha"
-                    required
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                />
-
-                <Button type="submit" color="#8ee88c">
-                    Entrar
-                </Button>
-            </form>
-        </div>
+                    <TextBox
+                        name="password"
+                        type="password"
+                        label="Senha"
+                        required
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                    />
+                    <Button type="submit" color="#8ee88c">
+                        Entrar
+                    </Button>
+                </form>
+            </div>
+        </>
     );
 }
