ce7a818186460c635db146b3ed1dd1cedcd1586d
mikhael <mikhael.dcs@gmail.com>
2019-12-08 17:21:56 -0300

    api host variable setup

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:3001' });
+const api = axios.create({ baseURL: `http://${process.env.backend}:3001` });
 
 export default api;
