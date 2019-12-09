1a1489d5b491cbc80a6de237185c1fa5600ab425
mikhael <mikhael.dcs@gmail.com>
2019-12-08 22:35:33 -0300

    docker enviroment setup

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
+require('dotenv').config();
 
-const api = axios.create({ baseURL: `http://${process.env.backend}:3001` });
+const api = axios.create({
+    baseURL: `http://localhost:3001`,
+    proxy: {
+        host: '172.23.0.3',
+        port: 3001,
+        },
+    });
 
 export default api;
