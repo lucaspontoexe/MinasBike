053c8c453c3d4789ece6430c5e75c6b01360f823
lucaspontoexe <ltx16@hotmail.com>
2019-12-09 01:07:37 -0300

    Incluindo suporte a SASS/SCSS seguindo com mais links no menu lateral

--- front/src/components/Menu/index.jsx
+++ front/src/components/Menu/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import './styles.css';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 export default function Menu() {
-
     // console.log(history.location)
 
     return (
         <div className="main-menu">
             <nav>
-                <Link to="/lista">Lista</Link>
+                <ul>
+                    <li>
+                        <Link to="/lista">Lista</Link>
+                    </li>
+                    <li>
+                        <Link to="/novo">Outro Link</Link>
+                    </li>
+                </ul>
             </nav>
         </div>
     );
