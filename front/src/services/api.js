5a39db8368c3a3be5b2549385bde41b6ecba1855
mikhael <mikhael.dcs@gmail.com>
2019-12-09 00:41:40 -0300

    finishing docker setup

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: `http://localhost:3001`,
     proxy: {
-        host: '172.23.0.3',
+        host: 'backend',
         port: 3001,
         },
     });
