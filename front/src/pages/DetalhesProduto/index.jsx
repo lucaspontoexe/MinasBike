0955faba7581c5ff3cfb7f590df6bedc8d0dfe6a
lucaspontoexe <ltx16@hotmail.com>
2019-12-12 23:03:33 -0300

    Estilização inicial do formulário

--- front/src/pages/DetalhesProduto/index.jsx
+++ front/src/pages/DetalhesProduto/index.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import TextBox from '../../components/TextBox'
+import React from 'react';
+import TextBox from '../../components/TextBox';
+import Header from '../../components/Header';
+import './styles.css';
 
 export default function DetalhesProduto(props) {
-    const {id} = props.match.params;
+    const { id } = props.match.params;
     return (
         <div className="tela detalhes-produto">
-            detalhes do produto {id}
-            <TextBox name="preco_custo" label="Preço de custo"/>
-            <TextBox name="preco_venda" label="Preço de venda"/>
-            <TextBox name="name" label="Nome do Produto"/>
-            <TextBox name="provider" label="Fornecedor"/>
-            <TextBox name="category" label="Categoria"/>
-            <TextBox name="sold" label="Quantidade Vendida"/>
-            <TextBox name="code" label="Código de Barras"/>
+            <Header>Detalhes do Produto {id}</Header>
+
+            <form action="">
+                <div className="column">
+                    <TextBox name="preco_custo" label="Preço de custo" />
+                    <TextBox name="preco_venda" label="Preço de venda" />
+                    <TextBox name="name" label="Nome do Produto" />
+                    <TextBox name="provider" label="Fornecedor" />
+                </div>
+
+                <div className="column">
+                    <TextBox name="category" label="Categoria" />
+                    <TextBox name="sold" label="Quantidade Vendida" />
+                    <TextBox name="code" label="Código de Barras" />
+                </div>
+            </form>
         </div>
-    )
+    );
 }
