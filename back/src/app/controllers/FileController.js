6300b0aab37fbecdde9a7f1c6a0063195628604f
mikhael <mikhael.dcs@gmail.com>
2019-12-13 18:23:34 -0300

    file initial setup

--- back/src/app/controllers/FileController.js
+++ back/src/app/controllers/FileController.js
@@ -1,6 +1,12 @@
+import xlsx from 'node-xlsx';
+
 class FileController {
   async store(req, res) {
-    res.json(req.file);
+    // const thefile = xlsx.parse(/tmp/uploads/36d049a3ec98d644aabaa15bbc76b092.csv);
+    const workSheetsFromBuffer = xlsx.parse(
+      `${__dirname}/../../../tmp/uploads/36d049a3ec98d644aabaa15bbc76b092.csv`
+    );
+    return res.json(workSheetsFromBuffer);
   }
 }
 
