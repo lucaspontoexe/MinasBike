d005a4a593824a2169d34200ec9e6d64d4b33f57
lucaspontoexe <ltx16@hotmail.com>
2019-12-13 18:30:50 -0300

    carrega todos os produtos da api

--- front/src/pages/DetalhesProduto/index.jsx
+++ front/src/pages/DetalhesProduto/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TextBox from '../../components/TextBox';
 import Button from '../../components/Button';
 import Header from '../../components/Header';
+import api from '../../services/api';
 import './styles.css';
 
 export default function DetalhesProduto(props) {
     const { id } = props.match.params;
+    const [products, setProducts] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [productData, setProductData] = useState({});
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const response = await api.get('/products');
+        setProducts(response.data)
+        setProductData(products.filter(item => item.id === id)[0])
+        setIsLoaded(true);
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -16,22 +30,23 @@ export default function DetalhesProduto(props) {
         <div className="tela detalhes-produto">
             <Header>Detalhes do Produto {id}</Header>
 
-            <form onSubmit={handleSubmit}>
+            {isLoaded && <form onSubmit={handleSubmit}>
                 <div className="column">
-                    <TextBox name="preco_custo" label="Preço de custo" type="number"/>
-                    <TextBox name="preco_venda" label="Preço de venda" type="number"/>
-                    <TextBox name="name" label="Nome do Produto" />
-                    <TextBox name="provider" label="Fornecedor" />
+                    <TextBox name="price" label="Preço" type="number" disabled value={productData.price}/>
+                    {/* <TextBox name="preco_custo" label="Preço de custo" type="number"/> */}
+                    {/* <TextBox name="preco_venda" label="Preço de venda" type="number"/> */}
+                    <TextBox name="name" label="Nome do Produto" disabled value={productData.name}/>
+                    <TextBox name="provider" label="Fornecedor" disabled value={productData.id_provider}/>
                 </div>
 
                 <div className="column">
-                    <TextBox name="category" label="Categoria" />
-                    <TextBox name="sold" label="Quantidade Vendida" />
-                    <TextBox name="code" label="Código de Barras" type="number"/>
+                    <TextBox name="category" label="Categoria" disabled value={productData.id_category}/>
+                    {/* <TextBox name="sold" label="Quantidade Vendida" /> */}
+                    <TextBox name="code" label="Código de Barras" type="number" disabled value={productData.code}/>
                 </div>
             </form>
 
-            <div className="buttons">
+            }{/* <div className="buttons">
                 <Button
                     color="#DC2438"
                     onClick={() => this.props.history.replace('/produtos')}
@@ -41,7 +56,7 @@ export default function DetalhesProduto(props) {
                 <Button type="submit" color="#30CC57">
                     Salvar
                 </Button>
-            </div>
+            </div> */}
         </div>
     );
 }
