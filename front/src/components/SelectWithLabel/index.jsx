de3f8fd8de97a60ee28ec707cc5355f972fef96b
lucaspontoexe <ltx16@hotmail.com>
2019-12-14 18:31:31 -0300

    estilos da select box foram extraídos para um novo arquivo

--- front/src/components/SelectWithLabel/index.jsx
+++ front/src/components/SelectWithLabel/index.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import Select from 'react-select';
-
+import customStyles from './customStyles';
 import './styles.css';
 
 export default function SelectWithLabel(props) {
     const { name, label } = props;
-
-    const customStyles = {
-        container: provided => {
-            return { ...provided, height: 48, marginTop: 5 };
-        },
-        control: provided => {
-            return { ...provided, height: '100%' };
-        },
-    };
 
     return (
         <div className="select-wrapper">
