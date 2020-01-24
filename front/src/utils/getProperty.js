8c21c4974aeabd98887670fe732edf1581f3a2b5
lucaspontoexe <ltx16@hotmail.com>
2020-01-24 00:26:07 -0300

    corrigindo código do getProperty

--- front/src/utils/getProperty.js
+++ front/src/utils/getProperty.js
@@ -1,5 +1,5 @@
 export default function getProperty(objects, id, property) {
-    const matches = objects.filter(obj => obj.id === id)[0];
+    const matches = objects.filter(obj => obj.id === id);
     if (matches.length === 0) return undefined;
-    return matches[property];
+    return matches[0][property];
 }
