834c1f80a9707e059860c6ebb80db951da0c196a
lucaspontoexe <ltx16@hotmail.com>
2020-01-28 19:39:56 -0300

    inputs adicionados

--- front/src/pages/Cadastro/Fornecedor.jsx
+++ front/src/pages/Cadastro/Fornecedor.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import Header from 'components/Header'
+import React from 'react';
+import Header from 'components/Header';
+import TextBox from 'components/TextBox';
+import SelectWithLabel from 'components/SelectWithLabel';
 
 export default function CadastroFornecedor() {
     return (
-        <div>
+        <div className="tela tela-cadastro">
             <Header>Novo Fornecedor</Header>
-            
+
+            <TextBox name="name" label="Nome do Fornecedor" required />
+            <SelectWithLabel name="location" label="Cidade - Estado" required />
+            <TextBox
+                name="name"
+                label="Nome do Contato Principal"
+                required
+            />
+            <TextBox
+                name="phone"
+                label="Telefone do contato"
+                type="tel"
+                required
+            />
+            <TextBox
+                name="email"
+                label="E-mail do contato principal"
+                type="email"
+                required
+            />
         </div>
-    )
+    );
 }
