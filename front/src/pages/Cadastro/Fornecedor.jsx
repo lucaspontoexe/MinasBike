e9f80a7f2e3a1d8d6ecf78f57d2b56efa1bf0c91
lucaspontoexe <ltx16@hotmail.com>
2020-02-01 22:16:44 -0300

    seletor de cidade/estado agora organiza por estado

--- front/src/pages/Cadastro/Fornecedor.jsx
+++ front/src/pages/Cadastro/Fornecedor.jsx
@@ -1,23 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from 'services/api';
+import useAuth from 'utils/useAuth';
+import stateNames from './states.json';
+
 import Header from 'components/Header';
 import TextBox from 'components/TextBox';
 import SelectWithLabel from 'components/SelectWithLabel';
-import api from 'services/api';
-import useAuth from 'utils/useAuth';
 import Button from 'components/Button';
 
-export default function CadastroFornecedor({history}) {
+export default function CadastroFornecedor({ history }) {
     //TODO: Procurar se alguma lib de form ajuda
     const [formData, setFormData] = useState({});
+    const [currentBRState, setCurrentBRState] = useState('');
+    const [currentCity, setCurrentCity] = useState({});
+    const [cityList, setCityList] = useState([]);
 
     function handleSubmit(event) {
         event.preventDefault();
         api.post('/providers', useAuth, formData);
     }
 
+    useEffect(() => {
+        setCurrentCity({})
+        api.get('/locations', {
+            ...useAuth,
+            params: { state: currentBRState },
+        }).then(res =>
+            setCityList(
+                res.data.map(item => {
+                    return { value: item.id, label: item.city };
+                })
+            )
+        );
+    }, [currentBRState]);
+
     function handleChange({ target }) {
         setFormData({ ...formData, [target.name]: target.value });
     }
+
+    const br_states = stateNames.map(item => {
+        return { value: item, label: item };
+    });
+
     return (
         <div className="tela tela-cadastro">
             <Header>Novo Fornecedor</Header>
@@ -30,10 +55,21 @@ export default function CadastroFornecedor({history}) {
                     onChange={handleChange}
                 />
                 <SelectWithLabel
-                    name="location"
-                    label="Cidade - Estado"
-                    // required
+                    name="location_state"
+                    label="Estado"
+                    required
+                    options={br_states}
+                    onChange={opt => setCurrentBRState(opt.value)}
+                />
+                <SelectWithLabel
+                    name="location_city"
+                    label="Cidade"
+                    required
+                    isDisabled={currentBRState === ''}
+                    options={cityList}
+                    value={currentCity}
                     onChange={opt => {
+                        setCurrentCity(opt);
                         setFormData({ ...formData, location_id: opt.value });
                     }}
                 />
@@ -61,9 +97,7 @@ export default function CadastroFornecedor({history}) {
                     <Button
                         type="reset"
                         color="#DC2438"
-                        onClick={() =>
-                            history.replace('/fornecedores')
-                        }
+                        onClick={() => history.replace('/fornecedores')}
                     >
                         Cancelar
                     </Button>
