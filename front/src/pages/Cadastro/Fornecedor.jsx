168174e56142c413867729cd926e12d4d7824a7b
lucaspontoexe <ltx16@hotmail.com>
2020-02-01 22:26:17 -0300

    escolha de localização movida para outro componente

--- front/src/pages/Cadastro/Fornecedor.jsx
+++ front/src/pages/Cadastro/Fornecedor.jsx
@@ -1,47 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import api from 'services/api';
 import useAuth from 'utils/useAuth';
-import stateNames from './states.json';
 
 import Header from 'components/Header';
 import TextBox from 'components/TextBox';
-import SelectWithLabel from 'components/SelectWithLabel';
 import Button from 'components/Button';
+import LocationSelect from 'components/LocationSelect/index.jsx';
 
 export default function CadastroFornecedor({ history }) {
     //TODO: Procurar se alguma lib de form ajuda
     const [formData, setFormData] = useState({});
-    const [currentBRState, setCurrentBRState] = useState('');
-    const [currentCity, setCurrentCity] = useState({});
-    const [cityList, setCityList] = useState([]);
 
     function handleSubmit(event) {
         event.preventDefault();
         api.post('/providers', useAuth, formData);
     }
 
-    useEffect(() => {
-        setCurrentCity({})
-        api.get('/locations', {
-            ...useAuth,
-            params: { state: currentBRState },
-        }).then(res =>
-            setCityList(
-                res.data.map(item => {
-                    return { value: item.id, label: item.city };
-                })
-            )
-        );
-    }, [currentBRState]);
-
     function handleChange({ target }) {
         setFormData({ ...formData, [target.name]: target.value });
     }
-
-    const br_states = stateNames.map(item => {
-        return { value: item, label: item };
-    });
 
     return (
         <div className="tela tela-cadastro">
@@ -54,24 +32,10 @@ export default function CadastroFornecedor({ history }) {
                     required
                     onChange={handleChange}
                 />
-                <SelectWithLabel
-                    name="location_state"
-                    label="Estado"
-                    required
-                    options={br_states}
-                    onChange={opt => setCurrentBRState(opt.value)}
-                />
-                <SelectWithLabel
-                    name="location_city"
-                    label="Cidade"
-                    required
-                    isDisabled={currentBRState === ''}
-                    options={cityList}
-                    value={currentCity}
-                    onChange={opt => {
-                        setCurrentCity(opt);
-                        setFormData({ ...formData, location_id: opt.value });
-                    }}
+                <LocationSelect
+                    onChange={value =>
+                        setFormData({ ...formData, location_id: value })
+                    }
                 />
                 <TextBox
                     name="contact"
