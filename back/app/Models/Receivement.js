5043cc0630f3f5f59a1ad16f439158e285eb7686
Mikhael Dantas <mikhael.dcs@gmail.com>
2020-02-03 20:56:47 -0300

    implementation of the receivements with relations

--- back/app/Models/Receivement.js
+++ back/app/Models/Receivement.js
@@ -3,27 +3,19 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Providerproduct extends Model {
-  provider () {
-    return this.belongsTo('App/Models/Provider')
-  }
-
-  brandproduct () {
-    return this.belongsTo('App/Models/Brandproduct')
-  }
-
+class Receivement extends Model {
   receivedproviderproducts () {
     return this.hasMany('App/Models/Receivedproviderproduct')
   }
 
-  receivements () {
+  providerproducts () {
     return this.manyThrough(
       'App/Models/Receivedproviderproduct',
-      'receivement',
+      'providerproduct',
       'id',
-      'providerproduct_id'
+      'receivement_id'
     )
   }
 }
 
-module.exports = Providerproduct
+module.exports = Receivement
