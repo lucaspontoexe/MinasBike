73ff558a3e213fbf28d87b2ea457319dffe63900
lucaspontoexe <ltx16@hotmail.com>
2020-03-04 18:53:47 -0300

    load values from API

--- front/src/pages/Cadastro/Produto/BPSelector.jsx
+++ front/src/pages/Cadastro/Produto/BPSelector.jsx
@@ -1,16 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import SelectWithLabel from 'components/SelectWithLabel';
-// import Modal from 'components/Modal';
-// import './styles.css';
+import api from 'services/api';
+
 export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
   // load all products and brands on init
   // more data will be loaded as the form is filled
+  const [brands, setBrands] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/products').then(res => setProducts(res.data));
+    api.get('/brands').then(res => setBrands(res.data));
+  }, [])
+  
   const someOptions = [
     { value: 1, label: 'one' },
     { value: 2, label: 'two' },
     { value: 3, label: 'three' },
     { value: 4, label: 'four' },
   ];
+
+  function handleChange(value, {name}) {
+      console.log(value, name)
+  }
+
+
   return (
     <Fragment>
       <fieldset>
@@ -20,7 +34,7 @@ export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
           creatable
           options={someOptions}
           onCreateOption={console.log}
-          onChange={console.log}
+          onChange={handleChange}
           required
           label="Nome do Produto"
           placeholder="nome do produto"
@@ -30,7 +44,7 @@ export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
           creatable
           options={someOptions}
           onCreateOption={console.log}
-          onChange={console.log}
+          onChange={handleChange}
           required
           label="Nome da Marca"
           placeholder="nome da marca"
