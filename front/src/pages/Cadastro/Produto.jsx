8d2ba7286b3de9fbfab048a3b846ed76d455962b
lucaspontoexe <ltx16@hotmail.com>
2020-03-04 11:12:30 -0300

    textbox -> select anotações adicionadas

--- front/src/pages/Cadastro/Produto.jsx
+++ front/src/pages/Cadastro/Produto.jsx
@@ -4,7 +4,7 @@ import TextBox from 'components/TextBox';
 // import Button from 'components/Button';
 import Header from 'components/Header';
 // import Error from 'components/Error';
-// import SelectWithLabel from 'components/SelectWithLabel';
+import SelectWithLabel from 'components/SelectWithLabel';
 // import Modal from 'components/Modal';
 
 import './styles.css';
@@ -13,20 +13,21 @@ export default function CadastroProduto() {
   return (
     <div className="tela">
       <Header>Novo Produto</Header>
-
+      <fieldset>
+        BP selector
+        <SelectWithLabel required label="Nome do Produto" placeholder="nome do produto" />
+        <SelectWithLabel required label="Nome da Marca" placeholder="nome da marca" />
+      </fieldset>
+      <pre>current BP: Tal Produto; ID: 1 (ou [to be created])</pre>
+      <br/>
+      
       <fieldset>
         PRODUCT STUFF
-        {/* BP selector */}
-        {/* na real, um select */}
-        <TextBox required label="Nome do Produto" placeholder="nome do produto" />
-        <TextBox required label="Nome da Marca" placeholder="nome da marca" />
-        <pre>current BP: Tal Produto; ID: 1</pre>
-        {/* end bp selector */}
         <TextBox required label="Descrição" />
         <TextBox required label="Unidade de Medida" />
         <TextBox required label="Categoria" />
       </fieldset>
-
+      
       <fieldset>
         BP STUFF
         <TextBox required label="Código de Barras" />
@@ -34,14 +35,14 @@ export default function CadastroProduto() {
       </fieldset>
 
       <fieldset>
-      STOCK STUFF
-      <TextBox required label="qtd. atual estoque" />
-      <TextBox required label="qtd. mínima estoque" />
-      <TextBox required label="qtd. inicial em estoque" />
+        STOCK STUFF <i>(requires brandproduct)</i>
+        <TextBox required label="qtd. atual estoque" />
+        <TextBox required label="qtd. mínima estoque" />
+        <TextBox required label="qtd. inicial em estoque" />
       </fieldset>
-
+      
       PROVIDER STUFF
-      <input placeholder="json data"/>
+      <input placeholder="json data" />
     </div>
   );
 }
