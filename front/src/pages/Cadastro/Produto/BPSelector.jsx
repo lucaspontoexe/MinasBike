59fa8ec7cfde4afc8057e8b5a0e34237a5c661cc
lucaspontoexe <ltx16@hotmail.com>
2020-03-05 10:46:56 -0300

    um state pra cada ID

--- front/src/pages/Cadastro/Produto/BPSelector.jsx
+++ front/src/pages/Cadastro/Produto/BPSelector.jsx
@@ -3,21 +3,34 @@ import SelectWithLabel from 'components/SelectWithLabel';
 import api from 'services/api';
 
 export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
+  // single state for each property (2nd attempt)
+  const [brand_id, setBrandID] = useState(-1);
+  const [product_id, setProductID] = useState(-1);
+  const [brandproduct_id, setBrandproductID] = useState(-1);
+
   // load all products and brands on init
   // more data will be loaded as the form is filled
   const [brands, setBrands] = useState([]);
   const [products, setProducts] = useState([]);
 
-  const formatSelectItem = item => ({ value: item.id, label: item.name });
-
+  // grab from API
   useEffect(() => {
+    const formatSelectItem = item => ({ value: item.id, label: item.name });
     api.get('/products').then(res => setProducts(res.data.map(formatSelectItem)));
     api.get('/brands').then(res => setBrands(res.data.map(formatSelectItem)));
   }, []);
 
-  function handleChange(value, { name }) {
-    console.log(value, name);
-  }
+  useEffect(() => {
+    if (brand_id === -1 || product_id === -1) return;
+
+    function formatBrandproductID(array) {
+      if (array.length === 0) return -1;
+      return array[0].id;
+    }
+    api
+      .get('/brandproducts', { params: { brand_id, product_id } })
+      .then(res => setBrandproductID(formatBrandproductID(res.data)));
+  }, [brand_id, product_id]);
 
   return (
     <Fragment>
@@ -26,9 +39,9 @@ export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
         <SelectWithLabel
           name="product"
           creatable
-          options={brands}
+          options={products}
           onCreateOption={console.log}
-          onChange={handleChange}
+          onChange={({ value: id }) => setProductID(id)}
           required
           label="Nome do Produto"
           placeholder="nome do produto"
@@ -36,15 +49,19 @@ export function BPSelector({ onProductChange, onBrandChange, onBPChange }) {
         <SelectWithLabel
           name="brand"
           creatable
-          options={products}
+          options={brands}
           onCreateOption={console.log}
-          onChange={handleChange}
+          onChange={({ value: id }) => setBrandID(id)}
           required
           label="Nome da Marca"
           placeholder="nome da marca"
         />
       </fieldset>
-      <pre>current BP: Tal Produto; ID: 1 (ou [to be created])</pre>
+      <pre>
+        current product: {product_id} <br />
+        current brand: {brand_id} <br />
+        current BP: {brandproduct_id === -1 ? '(to be created)' : brandproduct_id}
+      </pre>
       <br />
     </Fragment>
   );
