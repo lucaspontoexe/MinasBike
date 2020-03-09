eb9d4d91f688f8b3e7a217061857d37507501e2d
lucaspontoexe <ltx16@hotmail.com>
2020-03-09 00:37:51 -0300

    regra 1 tá quase implementada

--- front/src/pages/Cadastro/Produto/index.jsx
+++ front/src/pages/Cadastro/Produto/index.jsx
@@ -6,22 +6,45 @@ import Header from 'components/Header';
 import { BPSelector } from './BPSelector';
 
 export default function CadastroProduto() {
-  const [bpData, setBpData] = useState({})
+  const [bpData, setBpData] = useState({
+    product: { id: -2 },
+    brand: { id: -2 },
+    brandproduct: { id: -1 },
+  });
 
   return (
     <div className="tela">
       <Header>Novo Produto</Header>
-      <BPSelector onChange={setBpData}/>
+      <BPSelector onChange={setBpData} />
       <fieldset>
         PRODUCT STUFF
-        <TextBox required label="Descrição" />
-        <TextBox required label="Unidade de Medida" />
-        <TextBox required label="Categoria" />
+        {bpData.product.id < 0 ? ( //empty?
+          <>
+            <TextBox required label="Descrição" />
+            <TextBox required label="Unidade de Medida" />
+            <TextBox required label="Categoria" />
+          </>
+        ) : (
+          <>
+            <TextBox disabled value={bpData.product.description} label="Descrição" />
+            <TextBox disabled value={bpData.product.unity_id} label="Unidade de Medida" />
+            <TextBox disabled value={bpData.product.category_id} label="Categoria" />
+          </>
+        )}
       </fieldset>
       <fieldset>
         BP STUFF
-        <TextBox required label="Código de Barras" />
-        <TextBox required label="Preço" />
+        {bpData.brandproduct.id < 0 ? (
+          <>
+            <TextBox required label="Código de Barras" />
+            <TextBox required label="Preço" />
+          </>
+        ) : (
+          <>
+            <TextBox disabled value={bpData.brandproduct.code} label="Código de Barras" />
+            <TextBox disabled value={bpData.brandproduct.price} label="Preço" />
+          </>
+        )}
       </fieldset>
       <fieldset>
         STOCK STUFF <i>(requires brandproduct)</i>
@@ -31,7 +54,6 @@ export default function CadastroProduto() {
       </fieldset>
       PROVIDER STUFF <i>(requires brandproduct)</i>
       <input placeholder="json data" />
-
       <Button>POST</Button>
     </div>
   );
