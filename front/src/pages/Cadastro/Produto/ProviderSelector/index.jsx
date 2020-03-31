04d4ec597c2df0c03cfb08e0073f48aef021489b
lucaspontoexe <ltx16@hotmail.com>
2020-03-31 10:16:46 -0300

    rename function

--- front/src/pages/Cadastro/Produto/ProviderSelector/index.jsx
+++ front/src/pages/Cadastro/Produto/ProviderSelector/index.jsx
@@ -25,11 +25,11 @@ export default function ProviderSelector({ brandproduct_id, onChange }) {
   }, [brandproduct_id]);
 
   useEffect(() => {
-    onChange({ items: newitems, isValid: validate(), teste: 'umdois' });
+    onChange({ items: newitems, isValid: checkIfValid(), teste: 'umdois' });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [newitems, onChange]);
 
-  const validate = () => {
+  const checkIfValid = () => {
     const prprIDList = prpr.map(item => item.provider_id);
     const newitemsIDList = newitems.map(item => Number(item.provider_id));
     const temp = prprIDList.concat(newitemsIDList)
