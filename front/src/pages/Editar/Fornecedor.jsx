93553d6aecd0d7cc3163659fad803798e4d0590c
lucaspontoexe <ltx16@hotmail.com>
2020-04-06 15:52:15 -0300

    rip formik

--- front/src/pages/Editar/Fornecedor.jsx
+++ front/src/pages/Editar/Fornecedor.jsx
@@ -1,41 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import api from 'services/api';
+
 import Header from 'components/Header';
-import { Formik, Form } from 'formik';
+import TextBox from 'components/TextBox';
 import Button from 'components/Button';
+import LocationSelector from 'components/LocationSelector';
 
-export default function EditarFornecedor(props) {
+export default function EditarFornecedor(props, { history }) {
   const { id } = props.match.params;
+  const [formData, setFormData] = useState({});
 
-  const [providerData, setProviderData] = useState({});
-  const [isLoaded, setIsLoaded] = useState(false);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await api.put(`/providers/${id}`, formData);
+    history.push('/fornecedores');
+  }
 
-  useEffect(() => {
-    api.get('/providers', { params: { id } }).then(response => {
-      setProviderData(response.data[0]);
-      setIsLoaded(true);
-    });
-  }, [id]);
+  function handleChange({ target }) {
+    setFormData({ ...formData, [target.name]: target.value });
+  }
+
+  // Formik não parece ser tão interessante pra essa página.
 
   return (
-    <div className="tela detalhes-fornecedor">
-      <Header>Editar Fornecedor</Header>
+    <div className="tela tela-cadastro">
+      <Header>Novo Fornecedor</Header>
 
-      <Formik initialValues={providerData}>
-        <Form>
-          {/* FormikInput */}
-
-          <div className="buttons">
-            <Button type="reset" color="#DC2438" onClick={() => props.history.pop()}>
-              Voltar
-            </Button>
-            <Button type="submit" color="#30CC57">
-              Salvar
-            </Button>
-            <pre>is loaded: {JSON.stringify(isLoaded)}</pre>
-          </div>
-        </Form>
-      </Formik>
+      <form action="#" onSubmit={handleSubmit}>
+        <TextBox name="name" label="Nome do Fornecedor" required onChange={handleChange} />
+        <LocationSelector
+          required
+          onChange={value => setFormData({ ...formData, location_id: value })}
+        />
+        <TextBox
+          name="contact"
+          label="Nome do Contato Principal"
+          required
+          onChange={handleChange}
+        />
+        <TextBox
+          name="phone"
+          label="Telefone do contato"
+          type="tel"
+          required
+          onChange={handleChange}
+        />
+        <TextBox
+          name="email"
+          label="E-mail do contato principal"
+          type="email"
+          required
+          onChange={handleChange}
+        />
+        <div className="buttons">
+          <Button type="reset" color="#DC2438" onClick={() => history.replace('/fornecedores')}>
+            Cancelar
+          </Button>
+          <Button type="submit" color="#30CC57">
+            Cadastrar
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
