6a645f230ff95840afad6f2fb76810b7eb3f1248
lucaspontoexe <ltx16@hotmail.com>
2020-05-10 16:20:28 -0300

    componentes & campos incluídos

--- front/src/pages/Editar/Produto.jsx
+++ front/src/pages/Editar/Produto.jsx
@@ -5,41 +5,80 @@ import { formatErrorsSingleObject } from 'utils/formatFieldErrors';
 import Header from 'components/Header';
 import TextBox from 'components/TextBox';
 import Button from 'components/Button';
-
+import SelectWithLabel from 'components/SelectWithLabel';
+import ProviderSelector from 'pages/Cadastro/Produto/ProviderSelector';
 
 export default function EditarProduto(props) {
   const { id } = props.match.params;
   const [canEdit, setCanEdit] = useState(false);
-  
+
   return (
     <div className="tela tela-cadastro">
-      <Header>Editar Produto</Header>
-
+      <Header>Editar Produto {id}</Header>
       <Button color="#dc2438" onClick={() => setCanEdit(true)}>
         Editar
       </Button>
-
       <form action="#">
-
         {/* product stuff */}
-        <br/> nome
-        <br/> categoria
+        <TextBox
+          required
+          disabled={!canEdit}
+          name="name"
+          label="Nome do Produto"
+          // onChange={handleInputChange}
+          // error={errors.description}
+        />
+        <SelectWithLabel
+          required
+          isDisabled={!canEdit}
+          name="category"
+          label="Categoria"
+          // error={errors.category_id}
+          // options={categories.map(item => formatSelectItem(item.id, item.name))}
+          // onChange={sel => handleChange(setProductForm, { name: 'category_id', value: sel.value })}
+        />
 
         {/* brand */}
-        <br/> marca
-
+        <SelectWithLabel
+          required
+          isDisabled={!canEdit}
+          name="brand"
+          label="Marca"
+          // error={errors.category_id}
+          // options={categories.map(item => formatSelectItem(item.id, item.name))}
+          // onChange={sel => handleChange(setProductForm, { name: 'category_id', value: sel.value })}
+        />
 
         {/* brandproduct */}
-        <br/> código de barras
-        <br/> preço de custo
+        <TextBox
+          required
+          disabled={!canEdit}
+          name="code"
+          label="Código de Barras"
+          // onChange={handleInputChange}
+          // error={errors.description}
+        />
+
+        <TextBox
+          required
+          disabled={!canEdit}
+          name="cost_price"
+          label="Preço de Custo"
+          // onChange={handleInputChange}
+          // error={errors.description}
+        />
 
         {/* stock */}
-        <br/> quantidade atual de estoque
-
+        <TextBox
+          required
+          disabled={!canEdit}
+          name="current_qty"
+          label="Quantidade atual de estoque"
+          // onChange={handleInputChange}
+          // error={errors.description}
+        />
         {/* provider */}
-        <br/> import provider selector
-
-
+        <ProviderSelector brandproduct_id={1} onChange={console.log} />
       </form>
       canedit: {JSON.stringify(canEdit)}
     </div>
