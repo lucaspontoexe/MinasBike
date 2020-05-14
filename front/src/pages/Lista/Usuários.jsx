08ae0adea51d2205aa419b0eed6be20e089fb568
lucaspontoexe <ltx16@hotmail.com>
2020-05-14 11:38:14 -0300

    include usertype

--- front/src/pages/Lista/Usuários.jsx
+++ front/src/pages/Lista/Usuários.jsx
@@ -11,7 +11,7 @@ export default function ListaClientes({ history }) {
 
   useEffect(() => {
     function fetchData() {
-      api.get('/users').then(res => setUsers(res.data));
+      api.get('/users?usertype').then(res => setUsers(res.data));
     }
     fetchData();
   }, []);
@@ -19,15 +19,17 @@ export default function ListaClientes({ history }) {
   const headers = [
     { Header: 'Nome', accessor: 'name' },
     { Header: 'E-mail', accessor: 'email' },
-    { Header: 'Cargo', accessor: 'usertype_id' },
+    { Header: 'Cargo', accessor: 'usertype_name' },
     { Header: 'Situação', accessor: 'active' },
   ];
+
+  const data = users.map(user => ({ ...user, usertype_name: user.usertype.name }));
 
   return (
     <div className="tela tela--lista">
       <Header>Usuários</Header>
       <div className="table-wrapper">
-        <Table columns={headers} data={users} searchText="Buscar usuários..." />
+        <Table columns={headers} data={data} searchText="Buscar usuários..." />
       </div>
     </div>
   );
