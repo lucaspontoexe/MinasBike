ed3e6788141e61b6712db57b839a4bf2f18fb557
lucaspontoexe <ltx16@hotmail.com>
2020-05-21 10:50:01 -0300

    ícone de edição adicionado à célula editável

--- front/src/components/Table/EditableCell.jsx
+++ front/src/components/Table/EditableCell.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 const EditableCell = ({
-    cell: { value: initialValue },
-    row: { index },
-    column: { id },
-    updateData, // This is a custom function that we supplied to our table instance
-  }) => {
-    // We need to keep and update the state of the cell normally
-    const [value, setValue] = React.useState(initialValue);
+  cell: { value: initialValue },
+  row: { index },
+  column: { id },
+  updateData, // This is a custom function that we supplied to our table instance
+}) => {
+  // We need to keep and update the state of the cell normally
+  const [value, setValue] = React.useState(initialValue);
 
-    const onChange = e => {
-      setValue(e.target.value);
-    };
-
-    // We'll only update the external data when the input is blurred
-    const onBlur = () => {
-      updateData(index, id, value);
-    };
-
-    // If the initialValue is changed external, sync it up with our state
-    React.useEffect(() => {
-      setValue(initialValue);
-    }, [initialValue]);
-
-    return <input value={value} onChange={onChange} onBlur={onBlur} />;
+  const onChange = e => {
+    setValue(e.target.value);
   };
 
-  export default EditableCell;
+  // We'll only update the external data when the input is blurred
+  const onBlur = () => {
+    updateData(index, id, value);
+  };
+
+  // If the initialValue is changed external, sync it up with our state
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  return <input value={value} type="number" onChange={onChange} onBlur={onBlur} className="editable-cell" />;
+};
+export default EditableCell;
