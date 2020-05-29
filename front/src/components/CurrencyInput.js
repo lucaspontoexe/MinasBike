af06b680a5844c909751d5e3d17dda2833057a59
lucaspontoexe <ltx16@hotmail.com>
2020-05-29 11:27:28 -0300

    fix input weird behavior

--- front/src/components/CurrencyInput.js
+++ front/src/components/CurrencyInput.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function CurrencyInput(props) {
   const { value = 0, onChange, ...rest } = props;
   const [val, setVal] = useState(value);
+  const [isBlurred, setIsBlurred] = useState(true);
 
   useEffect(() => {
     setVal(props.value);
@@ -14,13 +15,21 @@ function CurrencyInput(props) {
     onChange(result);
   }
 
+  function fixIfNeeded(n) {
+    const a = Math.round(n * 100);
+    if (a % 10 === 0 && !Number.isInteger(n)) return n.toFixed(2);
+    return n;
+  }
+
   return (
     <input
       {...rest}
       type="number"
       step="0.01"
-      value={Number(val / 100).toFixed(2)}
+      value={isBlurred ? fixIfNeeded(Number(val / 100)) : Number(val / 100)}
       onChange={handleChange}
+      onFocus={() => setIsBlurred(false)}
+      onBlur={() => setIsBlurred(true)}
     />
   );
 }
